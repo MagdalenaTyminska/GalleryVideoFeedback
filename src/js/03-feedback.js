ced77b1bb312499eba3cd1834c535b987fd58e58
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2022-12-14 19:14:30 +0100

    button

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,9 @@
 var throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
+const submitButton = document.querySelector('button[type=submit]');
+
+buttonStatus();
 
 if (localStorage.getItem('feedback-form-state') !== null) {
   form.elements.email.value = JSON.parse(
@@ -9,22 +12,32 @@ if (localStorage.getItem('feedback-form-state') !== null) {
   form.elements.message.value = JSON.parse(
     localStorage.getItem('feedback-form-state')
   ).message;
+  buttonStatus();
 }
 
 form.addEventListener('input', throttle(saveFormData, 500));
 
-function saveFormData(event) {
+function saveFormData() {
   const data = {
     email: form.elements.email.value,
     message: form.elements.message.value,
   };
 
   localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  buttonStatus();
 }
 
 form.addEventListener('submit', onSubmit);
 
-function onSubmit(event) {
+function onSubmit() {
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   localStorage.removeItem('feedback-form-state');
+}
+
+function buttonStatus() {
+  if (form.elements.email.value == '' || form.elements.message.value == '')
+    submitButton.disabled = true;
+  else {
+    submitButton.disabled = false;
+  }
 }
